c64541d8332d85f83b8ae9e394b81af9f3c13edc
Artur Semenas <uncelfuka@gmail.com>
2023-11-12 00:55:47 +0100

    Module #4.2. Task #4. Loop Keywords

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
-
+        int skip = 0;
+        for (int i = 1 ; i<=lastPrinted; i++){
+            if (skip==2){
+                skip=0;
+                continue;
+            }
+            skip++;
+            System.out.println(i);
+        }
     }
 }
